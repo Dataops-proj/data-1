762535bc357586cac38a7f225a56f3ea6f2810e2
Dataops-proj <132896523+Dataops-proj@users.noreply.github.com>
2023-06-02 16:24:10 +0530

    Update file

--- csv_notebook_2.py
+++ csv_notebook_2.py
@@ -8,17 +8,17 @@ from pyspark.sql.functions import col
 spark=SparkSession.builder.appName('DATA-OPS').getOrCreate()
 sc = spark.sparkContext
 
-client = hvac.Client(url='http://54.163.61.208:8200', token='s.2Ji56wKC2oxwnK4UelSDMFow')
+client = hvac.Client(url='None', token='None')
 s_s3_credentials = client.read('kv/data/data/s3_credentials')['data']['data']
 access_key = s_s3_credentials.get('access_key')
 secret_key = s_s3_credentials.get('secret_key')
-aws_region = 'us-east-1'
+aws_region = 'ap-south-1'
 
 sc._jsc.hadoopConfiguration().set('fs.s3a.access.key', access_key)
 sc._jsc.hadoopConfiguration().set('fs.s3a.secret.key', secret_key)
 sc._jsc.hadoopConfiguration().set('fs.s3a.endpoint', 's3.' + aws_region + '.amazonaws.com')
 
-df = spark.read.format('csv').options(header='True').load('s3://yellow-buckets//us-500.csv')#Validation-notempty 
+df = spark.read.format('csv').options(header='True').load('s3://red-buckets/us-500.csv')#Validation-notempty 
 df = df.filter(~col('first_name').isNull()).limit(100)
 df = df.filter(~col('last_name').isNull()).limit(100)
 
@@ -41,4 +41,4 @@ else:
    df = df.withColumn('address', df['address'].cast('string'))
    df = df.withColumn('city', df['city'].cast('string'))
    df = df.withColumn('FULLNAME', concat("first_name", "last_name"))
-df.write.mode('overwrite').format('csv').save('s3a://pink-buckets/one/')
+df.write.mode('overwrite').format('csv').save('s3a://blue-buckets/two/')
