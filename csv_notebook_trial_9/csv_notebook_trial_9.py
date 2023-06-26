270b633630914be1b67e39be2302538ca40460a6
Dataops-proj <132896523+Dataops-proj@users.noreply.github.com>
2023-06-26 11:48:50 +0530

    Update file

--- csv_notebook_trial_9/csv_notebook_trial_9.py
+++ csv_notebook_trial_9/csv_notebook_trial_9.py
@@ -73,7 +73,7 @@ try:
 		df = df.withColumn('address', df['address'].cast('string'))
 		df = df.withColumn('city', df['city'].cast('string'))
 		df = df.withColumn('FULLNAME', concat("first_name", "last_name"))
-	df.write.mode('overwrite').format('csv').save('s3a://blue-buckets/parquet_to_csv_1/')
+	df.write.mode('overwrite').format('orc').save('s3a://blue-buckets/parquet_to_orc/')
 	logging.info('Data written to S3 bucket successfully')
 	logging.info('Data processing pipeline completed.')
 except Exception as e:
