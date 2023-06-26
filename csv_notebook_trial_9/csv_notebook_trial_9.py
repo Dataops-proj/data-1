f1f5839c318bf9731e738c7d154120c7db834294
Dataops-proj <132896523+Dataops-proj@users.noreply.github.com>
2023-06-26 12:38:04 +0530

    Update file

--- csv_notebook_trial_9/csv_notebook_trial_9.py
+++ csv_notebook_trial_9/csv_notebook_trial_9.py
@@ -50,7 +50,7 @@ try:
 	sc._jsc.hadoopConfiguration().set('fs.s3a.endpoint', 's3.' + aws_region + '.amazonaws.com')
 
 	#Read data from S3 bucket
-	df = spark.read.format('orc').options(header='True').load('s3://red-buckets/us-500.orc')
+	df = spark.read.format('parquet').options(header='True').load('s3://red-buckets/us-500.parquet')
 	logging.info('Data loaded from S3 bucket successfully')
 
 	#Validation-notempty
@@ -73,7 +73,7 @@ try:
 		df = df.withColumn('address', df['address'].cast('string'))
 		df = df.withColumn('city', df['city'].cast('string'))
 		df = df.withColumn('FULLNAME', concat("first_name", "last_name"))
-	df.write.mode('overwrite').format('csv').save('s3a://blue-buckets/orc_to_csv/')
+	df.write.mode('overwrite').format('csv').save('s3a://blue-buckets/parquet_to_csv/')
 	logging.info('Data written to S3 bucket successfully')
 	logging.info('Data processing pipeline completed.')
 except Exception as e:
