45158fc8a59aaeb62f347201663e877d25914d48
Dataops-proj <132896523+Dataops-proj@users.noreply.github.com>
2023-07-14 17:38:02 +0530

    Update file

--- new_acc/new_acc.py
+++ new_acc/new_acc.py
@@ -46,8 +46,8 @@ try:
 
 	client = hvac.Client(url=url_dcp, token=token_dcp)
 	s_s3_credentials = client.read('kv/data/data/S3_credentials')['data']['data']
-	access_key = s_s3_credentials.get('access_key')
-	secret_key = s_s3_credentials.get('secret_key')
+	access_key = s_s3_credentials.get('aws_access_key_id')
+	secret_key = s_s3_credentials.get('aws_secret_access_key')
 	aws_region = 'ap-south-1'
 	logging.info('AWS S3 credentials authenticated from Hvac Vault')
 
