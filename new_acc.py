928300e62072f1e46a7ac22f2a450a4a00e3fa92
Dataops-proj <132896523+Dataops-proj@users.noreply.github.com>
2023-07-27 20:37:47 +0530

    Update file

--- new_acc.py
+++ new_acc.py
@@ -67,7 +67,7 @@ try:
 
 	logging.info('AWS S3 credentials and database authenticated from Hvac Vault')
 
-	df = spark.read.format('jdbc').option('url', 'jdbc:postgresql://dataops-db.cr5bcibr4zvb.ap-south-1.rds.amazonaws.com:5432/postgres').option('driver', 'org.postgresql.Driver.com').option('dbtable', 'us').option('user', username_s).option('password', password_s).load()
+	df = spark.read.format('jdbc').option('url', 'jdbc:postgresql://dataops-db.cr5bcibr4zvb.ap-south-1.rds.amazonaws.com:5432/postgres').option('driver', 'org.postgresql.Driver').option('dbtable', 'us').option('user', username_s).option('password', password_s).load()
 	logging.info(f'The file us-500.csv loaded successfully')
 
 	#Get the number of rows
