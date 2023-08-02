a71558275c1c4598503246c7717f28197ea2e937
Dataops-proj <132896523+Dataops-proj@users.noreply.github.com>
2023-08-02 23:14:15 +0530

    Update file

--- new_acc/new_acc.py
+++ new_acc/new_acc.py
@@ -90,8 +90,21 @@ try:
 
 	logging.info('Data Transformation completed successfully')
 
+	# Get database credentials from Vault
+	database_credentials_target = client.secrets.kv.v2.read_secret(mount_point = 'dataops', path='data/dataopsmysql.cr5bcibr4zvb.ap-south-1.rds.amazonaws.com')['data']['data']
+	username_t = database_credentials_target.get('username')
+	password_t = database_credentials_target.get('password')
+
+	#Writing the dataframe to database
+	df.write.format('jdbc').mode('overwrite').option('url', 'jdbc:postgresql://dataopsmysql.cr5bcibr4zvb.ap-south-1.rds.amazonaws.com:3306/postgres').option('driver', 'org.postgresql.Driver').option('dbtable', 's3_to_postgresql_test_august_2').option('user', username_t).option('password', password_t).save()
+
+	logging.info('Data written to RDS successfully')
+	logging.info('Data processing pipeline completed.')
+
+	logging.info('Data Transformation completed successfully')
+
 	# Move custom log file to S3 bucket
-	logs_credentials = lient.secrets.kv.v2.read_secret(mount_point = 'kv', path=data/{}')['data']['data']
+	logs_credentials = client.secrets.kv.v2.read_secret(mount_point = 'kv', path = 'data'/{}')['data']['data']
 	access_key = logs_credentials.get('aws_access_key_id')
 	secret_key = logs_credentials.get('aws_secret_access_key')
 	aws_region = 'ap-south-1'
