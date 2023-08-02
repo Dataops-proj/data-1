7833cf23efcd66cac7a3f53845a79af78af857e3
Dataops-proj <132896523+Dataops-proj@users.noreply.github.com>
2023-08-02 16:02:15 +0530

    Update file

--- new_acc/new_acc.py
+++ new_acc/new_acc.py
@@ -104,8 +104,8 @@ try:
 
 	#Move custom log file to S3 bucket
 	logs_credentials = client.read('kv/data/data/Logs_credentials')['data']['data']
-	access_key = Logs_credentials.get('aws_access_key_id')
-	secret_key = Logs_credentials.get('aws_secret_access_key')
+	access_key = logs_credentials.get('aws_access_key_id')
+	secret_key = logs_credentials.get('aws_secret_access_key')
 	aws_region = 'ap-south-1'
 	s3 = boto3.client('s3', aws_access_key_id= access_key, aws_secret_access_key= secret_key,region_name=aws_region)
 
